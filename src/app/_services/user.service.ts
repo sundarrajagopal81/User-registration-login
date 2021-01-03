2959a4327559e6f519f6054029a60a4807602996
Mercy <mercypremaranim@gmail.com>
2021-01-03 12:50:37 -0700

    angular initial check in

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,36 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient,HttpResponse,HttpHeaders  } from '@angular/common/http';
+
+import { User } from '@/_models';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<User[]>(`${config.apiUrl}/users`);
+    }
+
+    getById(id: number) {
+        return this.http.get(`${config.apiUrl}/users/${id}`);
+    }
+
+    register(user: User) {
+        return this.http.post(`${config.apiUrl}/users/register`, user);
+    }
+
+    update(user: User) {
+        return this.http.put(`${config.apiUrl}/users/${user.id}`, user);
+    }
+
+    delete(id: number) {
+        return this.http.delete(`${config.apiUrl}/users/${id}`);
+    }
+}
